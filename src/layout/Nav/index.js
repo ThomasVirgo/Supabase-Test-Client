ad3b1e38331f2117c4e91a871e1fadb31fbbd878
Tom Virgo <tom.virgo@Apollotyres.com>
2021-11-10 14:48:54 +0000

    add nav styling

--- src/layout/Nav/index.js
+++ src/layout/Nav/index.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { SupaBaseContext } from '../../context/supabase_client';
 import { useNavigate, NavLink } from 'react-router-dom';
+import './style.css'
 
 
 const Nav = () => {
@@ -14,13 +15,15 @@ const Nav = () => {
         }
     }
     return (
-        <div>
+        <div className='nav_container'>
             { user &&
             <>
                 <p>Welcome, {user.user_metadata.username}</p>
-                <NavLink to='/account'>Account</NavLink>
-                <NavLink to='/'>Home</NavLink>
-                <button onClick = {logout}>Logout</button>
+                <div className='nav_links'>
+                    <NavLink to='/account'>Account</NavLink>
+                    <NavLink to='/'>Home</NavLink>
+                    <button onClick = {logout}>Logout</button>
+                </div>
             </>
             }
         </div>
