31f5166db187e793822086eb6792199308d2e3b0
Tom Virgo <tom.virgo@Apollotyres.com>
2021-11-10 16:58:29 +0000

    add chat

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,6 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useContext } from 'react'
+import { SupaBaseContext } from '../../context/supabase_client'
 
 const Chat = () => {
+    const supabase = useContext(SupaBaseContext)
+    useEffect(()=>{
+        async function fecthData(){
+            const { data, error } = await supabase.from('profiles').select()
+            console.log('error', error);
+            console.log(data);
+        }
+        fecthData()
+    }, [supabase])
     return (
         <div>
             Chat 
