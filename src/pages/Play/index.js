b2e38ec9b53de55440c3d66f0a31aab5a216563b
Tom Virgo <tomcvirgo@gmail.com>
2021-11-23 12:27:58 +0000

    remove O and 0 from code to remove confusion

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -18,7 +18,7 @@ const Play = () => {
 
     function makeid(length) {
         let result = '';
-        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let characters = 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz123456789';
         let charactersLength = characters.length;
         for ( let i = 0; i < length; i++ ) {
           result += characters.charAt(Math.floor(Math.random() * charactersLength));
